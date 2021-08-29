644f2e4b17b6b34ab26bfa24747e901b0265c41e
BirukTena <89731252+BirukTena@users.noreply.github.com>
2021-08-29 16:24:19 +0300

    Add files via upload

---
+++ src/firebase/base.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCvj754p1_t2AV6rBgaF7NxHb9fnCHoEH0",
+  authDomain: "sleep-tracker.firebaseapp.com",
+  projectId: "sleep-tracker",
+  storageBucket: "sleep-tracker.appspot.com",
+  messagingSenderId: "1006356862639",
+  appId: "1:1006356862639:web:a142a5924de3a66d7ee65a",
+  measurementId: "G-DR5F3YJ2X9"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+export default app;
